48640f66a706c8ae4dfaff7eba68ac10c50812dd
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2017-06-07 12:46:18 +0300

    checked out

--- Versus/app/js/scripts/app.js
+++ Versus/app/js/scripts/app.js
@@ -6,55 +6,41 @@
  */
 
 
-var versusApp = angular.module('VersusApp', [
-    // 'ngRoute',
-    'mobile-angular-ui']);
+var versusApp = angular.module('VersusApp', ['ui.router', 'mobile-angular-ui']);
 
-// versusApp.config(function($routeProvider) {
-//     $routeProvider.when('/', {
-// 	controller: "ListCtrl as ctrl",
-// 	templateUrl: "js/views/pages/list.html"
-//     }).when('/new', {
-// 	controller: "NewVersusCtrl as ctrl",
-// 	templateUrl: "js/views/pages/new-versus.html"
-//     });   
-// });
-
-// versusApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+versusApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     
-    // For any unmatched url, redirect to /state1
-//     $urlRouterProvider.otherwise(function($injector, $location){
-//         var loginUrl= '/';
-//         $injector.invoke(function($window, $timeout, $state) { 
-//     	    $state.go('list', {}, {reload: true});
-//         });
-//         return true;
-//     });
+   // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise(function($injector, $location){
+        var loginUrl= '/';
+        $injector.invoke(function($window, $timeout, $state) { 
+    	    $state.go('list', {}, {reload: true});
+        });
+        return true;
+    });
     
     
-//     // Now set up the states
-//     $stateProvider
-// 	.state('list', {
-// 	    url: "/",	    
-// 	    controller: "ListCtrl as ctrl",
-// 	    templateUrl: "js/views/pages/list.html"
-// 	})
-// 	.state('newversus', {
-// 	    url: "/new",	    
-// 	    controller: "NewVersusCtrl as ctrl",
-// 	    templateUrl: "js/views/pages/new-versus.html"
-// 	});    
-// }]);
+    // Now set up the states
+    $stateProvider
+	.state('list', {
+	    url: "/",	    
+	    controller: "ListCtrl as ctrl",
+	    templateUrl: "js/views/pages/list.html"
+	});
+	// .state('matches', {
+	//     url: "/matches",	    
+	//     controller: "MatchesCtrl as ctrl",
+	//     templateUrl: "js/views/pages/matches.html"
+	// });    
+}]);
 
 
 
-// versusApp.run(function($rootScope){
-//    $rootScope.$on('$stateChangeStart', function(){
-//       $rootScope.$broadcast('$routeChangeStart');
-//    });
-    // });
-
-    
+versusApp.run(function($rootScope){
+   $rootScope.$on('$stateChangeStart', function(){
+      $rootScope.$broadcast('$routeChangeStart');
+   });
+});
 // redirect
 // versusApp.run(['$rootScope', '$state', function($rootScope, $state) {
 //     $rootScope.$on('$stateChangeStart', function(evt, to, params) {
