68ff20c7ce3255a43cf39f5742666115d58528c6
Mikhail <mikhail@MacBook-Pro-Mikhail.local>
2017-06-10 16:06:16 +0300

    versus bot added

--- Versus/app/js/scripts/services/services.js
+++ Versus/app/js/scripts/services/services.js
@@ -89,7 +89,9 @@ angular.module('VersusApp')
     }]).service('VersusContract_', ['AlertSrvc', function(AlertSrvc) {
 
 	// address of contract
-	var CONTRACT_ADDRESS = '0x9684744c20734d370C9232f7E47B17E8Fcc11FFE';
+	//
+	var CONTRACT_ADDRESS = '0x9684744c20734d370C9232f7E47B17E8Fcc11FFE';  // Ropsten NET
+	//var CONTRACT_ADDRESS = '0xce0b05a42131aa22dcc02461bbd225c958165a28';   // Local
 	var CONTRACT_ABI = JSON.parse('[{"constant":true,"inputs":[],"name":"likeFee","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getUserVersuses","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"pairId","type":"uint256"}],"name":"getVersus","outputs":[{"name":"","type":"uint256"},{"name":"","type":"bytes32"},{"name":"","type":"bytes32"},{"name":"","type":"bytes32"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"versusIds","type":"uint256[]"},{"name":"chosenA","type":"bool[]"}],"name":"submitPolls","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"feedIds","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getVersuses","outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"pairCounter","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"title","type":"bytes32"},{"name":"imageSrcA","type":"bytes32"},{"name":"imageSrcB","type":"bytes32"},{"name":"pollMaxNumber","type":"uint256"}],"name":"addVersus","outputs":[{"name":"","type":"uint256[]"}],"payable":true,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]');
 	
 	var service = this;
@@ -148,7 +150,7 @@ angular.module('VersusApp')
 		AlertSrvc.showLoading("Submitting transaction...", "Submitting transaction to blockchain. It can take several minutes...Please wait.");
 		var ratedPairs = JSON.parse(localStorage.getItem("pairs")) || [];
 		try {
-		    service.contract.submitPolls(ids, bools,function(error, result){
+		    service.contract.submitPolls(ids, bools, {gas: 1000000}, function(error, result){
 			AlertSrvc.endLoading();
 			if(!error) {
 			    console.log(result);
@@ -179,11 +181,11 @@ angular.module('VersusApp')
 	
 	service.addVersus = function(versus )  {
 	    return new Promise(function(resolve, reject) {
-		//AlertSrvc.showLoading("Submitting transaction...", "Submitting transaction to blockchain. It can take several minutes...Please wait.");
-		try {
-		    service.contract.addVersus.sendTransaction(versus.title, versus.imageSrcA, versus.imageSrcB, versus.pollMaxNumber, {value:web3.toWei(versus.cost,'ether')}, function(err, result) {
-			
-			if(err) {
+		AlertSrvc.alert("Submitting transaction...", "Submitting transaction to blockchain. It can take several minutes...Please wait." + versus.pollMaxNumber).then(function() {
+		    try {
+			service.contract.addVersus.sendTransaction(versus.title, versus.imageSrcA, versus.imageSrcB, versus.pollMaxNumber, {value:web3.toWei(versus.cost,'ether'), gas: 1329176}, function(err, result) {
+			    
+			    if(err) {
 			    AlertSrvc.alert('Error', err).then(function() {
 				reject(err);
 			    });
@@ -200,7 +202,7 @@ angular.module('VersusApp')
 		    });
 		}
 
-
+		});
 		
 		
 		    
